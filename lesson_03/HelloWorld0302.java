161037f018c34a956637c76eadcb9c022f5926fc
Yugo TANAKA <tanaka@neugate.co.jp>
2024-05-27 20:53:30 +0900

    [Add] Lesson_03

---
+++ lesson_03/HelloWorld0302.java
@@ -0,0 +1,11 @@
+/* 
+シンプルな
+HelloWorld0302クラス
+ */
+// 「/* */」は複数行コメント可
+public class HelloWorld0302 {
+    public static void main(String[] args){
+        // メッセージをコンソールに出力
+        System.out.println("Hello, World!");
+    }
+}
